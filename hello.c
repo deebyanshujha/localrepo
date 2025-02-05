128211443276a50e587a52c2c83347dd92680b72
Deebyanshu <jhadeebyanshu@gmail.com>
2025-02-05 20:19:03 +0530

    added an input method

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 
 int main(){
+    int a;
+    scanf("%d",&a);
     printf("How are you?");
-    printf("This is a new feature");
+    printf("This is a new feature ");
     return 0;
 }
